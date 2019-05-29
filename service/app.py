6814066feaf1e4901fe92e1fb3c6d89cfb135c9d
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-05-29 11:42:33 -0400

    Fixed docker image deployment

--- service/app.py
+++ service/app.py
@@ -7,38 +7,31 @@ except ImportError:
 import logging
 
 from flask import Flask
-from flask.logging import default_handler
 from flask_socketio import SocketIO
 
-from service import config
+from assemblyline.common import forge, log as al_log
 from service.sio.helper import HelperNamespace
 from service.sio.tasking import TaskingNamespace
 
-app = Flask("alsvc")
-app.logger.setLevel(60)  # This completely turns off the flask logger
+config = forge.get_config()
 
-socketio = SocketIO(app, async_mode="gevent" if not config.DEBUG else "threading")
+# Prepare the logger
+al_log.init_logging('svc')
+LOGGER = logging.getLogger('assemblyline.svc.socketio')
+LOGGER.info("SocketIO server ready to receive connections...")
+
+# Prepare the app
+app = Flask('svc-socketio')
+socketio = SocketIO(app, async_mode="gevent" if not config.ui.debug else "threading")
+
+# Loading the different namespaces
 socketio.on_namespace(HelperNamespace('/helper'))
 socketio.on_namespace(TaskingNamespace('/tasking'))
 
-config.LOGGER.info("Service server API ready for connections...")
-
-
-def main():
-    wlog = logging.getLogger('werkzeug')
-    wlog.setLevel(config.LOGGER.getEffectiveLevel())
-    app.logger.setLevel(config.LOGGER.getEffectiveLevel())
-    app.logger.removeHandler(default_handler)
-    for h in config.LOGGER.parent.handlers:
-        app.logger.addHandler(h)
-        wlog.addHandler(h)
-
-    app.jinja_env.cache = {}
-    host = '0.0.0.0'
-    port = '5003'
-    config.LOGGER.info(f"Listening on http://{host}:{port} ...")
-    socketio.run(app, host=host, port=int(port), debug=False)
-
 
 if __name__ == '__main__':
-    main()
+    app.logger.setLevel(60)
+    wlog = logging.getLogger('werkzeug')
+    wlog.setLevel(60)
+    # Run debug mode
+    socketio.run(app, host='0.0.0.0', port=5003, debug=False)
