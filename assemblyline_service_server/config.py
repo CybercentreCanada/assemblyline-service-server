249e1133acb386c26e7a075c40ba7e8ae6c2ea0f
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2022-01-24 19:58:43 +0000

    pass single redis connection from root

--- assemblyline_service_server/config.py
+++ assemblyline_service_server/config.py
@@ -6,10 +6,24 @@ from assemblyline.common import forge
 from assemblyline.common import log as al_log
 from assemblyline.common.version import BUILD_MINOR, FRAMEWORK_VERSION, SYSTEM_VERSION
 from assemblyline.remote.datatypes.counters import Counters
+from assemblyline.remote.datatypes import get_client
 from assemblyline_core.safelist_client import SafelistClient
 from assemblyline_core.tasking_client import TaskingClient
 
+
 config = forge.get_config()
+
+redis = get_client(
+    host=config.core.redis.nonpersistent.host,
+    port=config.core.redis.nonpersistent.port,
+    private=False,
+)
+
+redis_persist = get_client(
+    host=config.core.redis.persistent.host,
+    port=config.core.redis.persistent.port,
+    private=False,
+)
 
 #################################################################
 # Configuration
@@ -20,10 +34,7 @@ DEBUG = config.ui.debug
 VERSION = os.environ.get('ASSEMBLYLINE_VERSION', f"{FRAMEWORK_VERSION}.{SYSTEM_VERSION}.{BUILD_MINOR}.dev0")
 AUTH_KEY = os.environ.get('SERVICE_API_KEY', 'ThisIsARandomAuthKey...ChangeMe!')
 
-RATE_LIMITER = Counters(prefix="quota",
-                        host=config.core.redis.nonpersistent.host,
-                        port=config.core.redis.nonpersistent.port,
-                        track_counters=True)
+RATE_LIMITER = Counters(prefix="quota", host=redis, track_counters=True)
 
 # End of Configuration
 #################################################################
@@ -46,7 +57,7 @@ LOGGER.debug('Logger ready!')
 STORAGE = forge.get_datastore(config=config)
 FILESTORE = forge.get_filestore(config=config)
 LOCK = threading.Lock()
-TASKING_CLIENT = TaskingClient(datastore=STORAGE, filestore=FILESTORE)
+TASKING_CLIENT = TaskingClient(datastore=STORAGE, filestore=FILESTORE, redis=redis, redis_persist=redis_persist)
 SAFELIST_CLIENT = SafelistClient(datastore=STORAGE)
 # End global
 #################################################################
